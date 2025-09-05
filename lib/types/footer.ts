c81604a9c32e5f7f670843427af27d465fa16a2f
Ulli-m <u.mentori@hotmail.com>
2025-09-05 15:43:45 +0200

    init

---
+++ lib/types/footer.ts
@@ -0,0 +1,31 @@
+export type UpTo3<T> = [] | [T] | [T, T] | [T, T, T];
+
+export type SubLogoKind = 'text' | 'email' | 'phone';
+
+export interface SubLogoItem {
+   type: SubLogoKind;
+   content: string;
+}
+
+export interface NavItem {
+   title: string;
+   link: string;
+}
+
+export interface NavGroup {
+   title: string;
+   items: readonly NavItem[];
+}
+
+export interface NewsletterConfig {
+   title: string;
+   description: string;
+   placeholder: string;
+   button: string;
+}
+
+export interface FooterConfig {
+   subLogoList: UpTo3<SubLogoItem>;
+   navGroups: UpTo3<NavGroup>;
+   newsLetter: NewsletterConfig;
+}
